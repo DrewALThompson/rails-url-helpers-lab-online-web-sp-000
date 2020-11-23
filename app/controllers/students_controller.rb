b344da04504b8555b1149a9c55868063da3a30f4
Drew Thompson <drewallynthompson@gmail.com>
2020-11-23 09:59:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
     set_student
   end
   
-  def activate
+  def activate_student_path
     set_student
     @student.active = !@student.active
     @student.save
